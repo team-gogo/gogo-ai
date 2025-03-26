ad9cc66881858080860200ee26439c680945e689
dongyeon <kdyeon0309@gmail.com>
2025-03-26 17:34:07 +0900

    Feat: Yolo11을 이용한 학습

---
+++ AI_relay/train.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO
+import os
+
+def main():
+    model = YOLO('yolo11n.pt')  
+
+    model.train(
+        data="AI_relay/yolo.yaml",
+        epochs=50,
+        imgsz=1024,
+        batch=10,
+        name='exp1',  
+        device='cuda',  
+        verbose=True
+    )
+
+  
+    weights_path = 'runs/train/exp1/weights/best.pt'
+    if os.path.exists(weights_path):
+        print(f"모델이 성공적으로 저장됨: {weights_path}")
+    else:
+        print("모델 저장 실패. 경로를 확인하세요.")
+
+    # 모델 검증
+    results = model.val()
+    print(results)
+
+
+if __name__ == '__main__':
+    os.environ['KMP_DUPLICATE_LIB_OK']='True'
+    main()
