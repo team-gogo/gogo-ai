00d7534516182a57e4993c31667c9eb720916765
dongyeon <kdyeon0309@gmail.com>
2025-03-30 06:52:57 +0900

    feat: ai 비속어 감지

---
+++ gogo_ai_backend/predict_model.py
@@ -0,0 +1,42 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+import pandas as pd
+import asyncio
+
+async def initialize_model_and_tokenizer(checkpoint_path, device):
+    model = AutoModelForSequenceClassification.from_pretrained(
+        checkpoint_path, local_files_only=True, trust_remote_code=True, use_auth_token=False
+    )
+    tokenizer = AutoTokenizer.from_pretrained("beomi/KcELECTRA-base")
+    model.to(device)
+    model.eval()
+    return model, tokenizer
+
+async def predict_sentence(model, tokenizer, sentence, device):
+    tokenized_input = tokenizer(
+        sentence,
+        return_tensors="pt",
+        truncation=True,
+        add_special_tokens=True,
+        max_length=128
+    ).to(device)
+
+    with torch.no_grad():
+        outputs = model(
+            input_ids=tokenized_input["input_ids"],
+            attention_mask=tokenized_input["attention_mask"],
+            token_type_ids=tokenized_input.get("token_type_ids")
+        )
+
+    logits = outputs.logits.detach().cpu()
+    prediction = logits.argmax(-1).item()
+    return prediction
+
+async def predictor(comment):
+    comment = comment
+    checkpoint_path = "./output/checkpoint-52350"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model, tokenizer = await initialize_model_and_tokenizer(checkpoint_path, device)
+    prediction= await predict_sentence(model, tokenizer, comment, device)
+    
+    return prediction
