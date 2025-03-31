eb8a4a42f8ff49029bac40abc93dbdf86508f423
dongyeon <kdyeon0309@gmail.com>
2025-03-31 16:22:51 +0900

    fix: 토픽 경로 수정

--- gogo_ai_backend/event/consumer.py
+++ gogo_ai_backend/event/consumer.py
@@ -10,18 +10,26 @@ from event.producer import EventProducer
 
 class filtered_result(BaseModel):
     id: str
-    boardId: Optional[int] = None
-    comment: Optional[int] = None
+    boardId: Optional[int] = None 
+    comment: Optional[int] = None  
+
+    class Config:
+        exclude_none = True
     
 events={
-    'board_create':'boardId', 'comment_create':'comment'
-}
+    'board_create':{
+        'output':'boardId',
+        'topic':'ai_board_filter'},
+    'comment_create':{
+        'output':'comment',
+        'topic':'ai_comment_filter'},
+    }
 
 async def consume():
     event=list(events.keys())
     consumer = AIOKafkaConsumer(
         event[0], event[1], 
-        bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}'
+        bootstrap_servers=f'{KAFKA_HOST}:{KAFKA_PORT}',
     )
 
     await consumer.start()
@@ -29,16 +37,21 @@ async def consume():
         async for msg in consumer:
             data = json.loads(msg.value.decode('utf-8'))
             logging.info(f'Consume kafka data {msg.topic} value: {data}')
+            if 'content' not in data:
+                logging.error(f"Missing 'content' key in message: {data}")
+                continue  # 'content' 키가 없으면 건너뜀
+            logging.info(f"data: {data['content']}")
             model_output = await predictor(data['content'])
             logging.info(f'Predictor output: {model_output}')
             await EventProducer.create_event(
-                topic=msg.topic,
+                topic=events[msg.topic]['topic'],
                 key=data['id'],
                 value=filtered_result(
                     id=data['id'],
-                    **{events[msg.topic]: model_output}
-                ),
+                    **{events[msg.topic]['output']: model_output}
+                ).dict(exclude_none=True),
             )
+            continue
 
     except Exception as e:
         logging.exception(f'Kafka consume exception {str(e)}')
