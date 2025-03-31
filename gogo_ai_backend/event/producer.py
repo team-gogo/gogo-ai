94c1949fa2e279b8bdf0d09c42f3965d7484e086
dongyeon <kdyeon0309@gmail.com>
2025-03-31 16:21:45 +0900

    refactor: value json dict 예외 처리

--- gogo_ai_backend/event/producer.py
+++ gogo_ai_backend/event/producer.py
@@ -1,6 +1,6 @@
 import json
 import logging
-from typing import Optional
+from typing import Optional, Union
 
 from aiokafka import AIOKafkaProducer
 from pydantic import BaseModel
@@ -20,11 +20,20 @@ class EventProducer:
         return EventProducer.producer
 
     @staticmethod
-    async def create_event(topic: str, key: str, value: BaseModel):
+    async def create_event(topic: str, key: str, value: Union[BaseModel, dict]):
+        """
+        - `value`가 `BaseModel`이면 `.dict()` 호출 후 JSON 변환
+        - `value`가 이미 `dict`라면 바로 JSON 변환
+        """
         producer = await EventProducer.get_producer()
+
+        if isinstance(value, BaseModel):
+            value = value.dict(exclude_none=True)  
+
         await producer.send_and_wait(
             key=key.encode('utf-8'),
             topic=topic,
-            value=json.dumps(value.dict()).encode('utf-8'),
+            value=json.dumps(value).encode('utf-8'), 
         )
-        logging.info(f'Kafka producer Send {topic} value: {value}')
+
+        logging.info(f'Kafka producer Send {topic} value: {json.dumps(value)}') 
