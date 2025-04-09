5964d6b1573ace7327d3c00e310143d60831ed6d
dongyeon <kdyeon0309@gmail.com>
2025-04-09 17:14:36 +0900

    유레카 설정 및 추가

--- gogo_ai_backend/server.py
+++ gogo_ai_backend/server.py
@@ -5,6 +5,7 @@ import uvicorn
 from middleware import LoggingMiddleware
 from event.consumer import consume
 from contextlib import asynccontextmanager
+from eureka import init_eureka
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,8 +24,8 @@ async def root():
  
 if __name__ == '__main__':
     try:
-        # init_eureka()
-        # logging.info('init eureka')
+        init_eureka()
+        logging.info('init eureka')
         uvicorn.run(app, host='0.0.0.0', port=8087, log_level='info', access_log=False)
     except Exception as e:
         print(e)
