56a6bda78489b233135425585c590fd0387f00e7
kdyeon0309 <kdyeon0309@gmail.com>
2025-05-02 00:52:41 +0900

    feat: 하이퍼파라미터 튜닝 코드

---
+++ AI_relay/hype_tuning.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov11m.pt')  
+
+model.tune(
+    data='./yolo.yaml',
+    epochs=5,              
+    iterations=50,   #하이퍼파라미터튜닝 횟수수
+    imgsz=1280,             
+    device='cuda',
+    batch=4,      
+    optimizer='Adam',        
+    plots=True              
+)
